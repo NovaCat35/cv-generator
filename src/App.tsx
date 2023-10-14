994ad09ce5f9af8d921e9ead7e85e6b6a6693e2c
Robert Chen <bergintonatcapital@gmail.com>
2023-10-14 02:19:17 -0400

    Create visible toggle btn for smaller screens and transition

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import GeneralForm from "./components/GeneralForm.tsx";
 import ResumePreview from "./components/ResumePreview.tsx";
 import EducationForm from "./components/EducationForm.tsx";
-import ExperienceForm from './components/ExperienceForm.tsx'
+import ExperienceForm from "./components/ExperienceForm.tsx";
 import "./styles/App.scss";
 import "./styles/mainForms.scss";
 import "./styles/resumePreview.scss";
+import "./styles/expandBtns.scss";
+import visibleImg from "./assets/visibility.svg";
+import visibleImgOff from "./assets/visibility_off.svg";
 
 export interface Person {
 	name: string;
@@ -23,7 +26,7 @@ export interface Education {
 export interface Experience {
 	company: string;
 	position: string;
-	description: string
+	description: string;
 	startDate: string;
 	endDate?: string;
 }
@@ -50,32 +53,46 @@ function App() {
 	});
 
 	const [experience, setExperience] = useState<Experience>({
-		company: 'Aperture Science',
-		position: 'Space Research Assistant',
-		description: 'Helped look into moon gel studies',
-		startDate: '09/1975',
-		endDate: '04/1980'
+		company: "Aperture Science",
+		position: "Space Research Assistant",
+		description: "Helped look into moon gel studies",
+		startDate: "09/1975",
+		endDate: "04/1980",
 	});
+
+	const [isPreviewActive, setIsPreviewActive] = useState(false);
 
 	/**
 	 * This handleChange function covers the dynamic inputs from each component 'Forms'.
-	 * The setState and currState is the parameters unique to each form component that. 
+	 * The setState and currState is the parameters unique to each form component that.
 	 * This way we can change the different states without having to create multiple handleChange for each 'form' component.
-	 * */ 
+	 * */
 	const handleChange = ({ e, fieldName, setState, currState }: HandleChange) => {
 		setState({ ...currState, [fieldName]: e.target.value });
 	};
 
 	return (
 		<>
-			<div className="main-forms-container">
+			<div className={isPreviewActive ? 'main-forms-container hidden' : 'main-forms-container'}>
 				<GeneralForm onChange={handleChange} currState={person} setState={setPerson} />
-				<EducationForm onChange={handleChange} currState={education} setState={setEducation}/>
-				<ExperienceForm onChange={handleChange} currState={experience} setState={setExperience}/>
+				<EducationForm onChange={handleChange} currState={education} setState={setEducation} />
+				<ExperienceForm onChange={handleChange} currState={experience} setState={setExperience} />
 			</div>
 			<div className="resume-preview">
 				<ResumePreview personInfo={person} educationInfo={education} experienceInfo={experience} />
 			</div>
+			<button
+				className={isPreviewActive ? "previewBtn active" : "previewBtn"}
+				onClick={() => {
+					if (isPreviewActive) {
+						setIsPreviewActive(false);
+					} else {
+						setIsPreviewActive(true);
+					}
+				}}
+			>
+				<img src={isPreviewActive ? visibleImgOff: visibleImg} alt="preview-btn" />
+			</button>
 		</>
 	);
 }
